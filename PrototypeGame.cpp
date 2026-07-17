d523cebce3823212f14ddec65f52acbf7347f709
FaisalMahir <mahirfaisal821@gmail.com>
2026-07-17 15:06:49 +0600

    merged with 'adding-bullets'

--- PrototypeGame.cpp
+++ PrototypeGame.cpp
@@ -6,41 +6,62 @@
 using namespace std;
 
 
-struct enemy{    
+struct Bullet {
+    Rectangle body;
+    float speedX, speedY;
+    bool active = true;
+    int targetId;
+    Texture2D *tex;
+
+    void update() {
+        body.x += speedX * GetFrameTime();
+        body.y += speedY * GetFrameTime();
+    }
+
+    void draw() {
+        float angle = atan2f(speedY, speedX) * (180.0f/3.1415926);
+        DrawTexturePro(*tex,{0, 0, (float)tex->width, (float)tex->height},body, {body.width / 2, body.height / 2},angle,WHITE);
+    }
+};
+
+
+struct enemy {    
     string s;
+    int currentIndex = 0;  
+
     Rectangle body;
     Rectangle text;
-    float posX , posY;
+    float posX, posY;
     float speedX, speedY;
-    
+
     int lev = 3;
+    int id;
+    int health;
+    bool active = true;
 
     Texture2D* tex;
     int frame = 0;
     float frameTimer = 0;
     float frameSpeed = 0.1f;
     int totalFrames = 16;
-    int cols = 4;           
-    int frameW = 200;       
-    int frameH = 200; 
-
+    int cols = 4;
+    int frameW = 200;
+    int frameH = 200;
 
     void update(){
-        body.x = body.x + speedX * GetFrameTime();
-        body.y = body.y + speedY * GetFrameTime();
-        text.x = text.x + speedX * GetFrameTime();
-        text.y = text.y + speedY * GetFrameTime();
-
+        body.x += speedX * GetFrameTime();
+        body.y += speedY * GetFrameTime();
+        text.x += speedX * GetFrameTime();
+        text.y += speedY * GetFrameTime();
 
         frameTimer += GetFrameTime();
-        if (frameTimer >= frameSpeed) {
+        if(frameTimer >= frameSpeed){
             frameTimer = 0;
             frame = (frame + 1) % totalFrames;
         }
-
     }
-    void draw(Color C){
 
+    void draw(Color C){
         int col = frame % cols;
         int row = frame / cols;
 
@@ -52,17 +73,17 @@ struct enemy{
         };
 
         DrawTexturePro(*tex, source, body, {0, 0}, 0.0f, WHITE);
-    
-        // DrawRectangleRec(body, WHITE);
         DrawRectangleRec(text, BLACK);
-        DrawText(s.c_str(), text.x + 5, text.y + 2, 20, C);
+        string typedPart   = s.substr(0, currentIndex);
+        string untypedPart = s.substr(currentIndex);
+        DrawText(typedPart.c_str(), text.x + 5, text.y + 2, 20, GRAY);
+        int typedWidth = MeasureText(typedPart.c_str(), 20);
+        DrawText(untypedPart.c_str(), text.x + 5 + typedWidth, text.y + 2, 20, C);
     }
-
 };
 
 
-struct BattlePlayer{
-
+struct Player {
     Rectangle body = {50, 500, 50, 50};
 
     void draw(){
@@ -70,53 +91,55 @@ struct BattlePlayer{
     }
 };
 
-void RunBattle(){
 
-    int level = 1;
+int main(){
 
-    float time=1.90; 
+    int level = 3;
+
+    float time = 1.90f;
     bool locked = false;
     int locked_index = -1;
 
-    SetWindowSize(800, 800); 
-    SetWindowTitle("Keyboard Warrior boutta be CRAAZZYYYY");
+    InitWindow(800, 800, "Keyboard Warrior boutta be CRAAZZYYYY");
     SetTargetFPS(60);
-    
-    
+
     float enemyspawner;
     float enemyBodySize;
     int enemyValue;
     Texture2D enemyTex;
     Texture2D bgTex;
+    Texture2D arrowTex;
     Color C;
 
-    if(level==1){
-        enemyTex = enemyTex = LoadTexture("resource/GreenDemonFinal.png");
-        bgTex = LoadTexture("resource/GreenBGFinal.png");
-        enemyspawner = 1.5;
+    if(level == 1){
+        enemyTex = LoadTexture("resource/GreenDemonFinal.png");
+        bgTex    = LoadTexture("resource/GreenBGFinal.png");
+        arrowTex = LoadTexture("resource/YellowArrow.png");
+        enemyspawner  = 1.5f;
         enemyBodySize = 80;
-        enemyValue = 2;
+        enemyValue    = 2;
         C = GREEN;
-    } 
-    else if(level==2){
-        enemyTex = enemyTex = LoadTexture("resource/BlueDemonFinal.png");
-        bgTex = LoadTexture("resource/BlueBGFinal.png");
-        enemyspawner = 1.3;
+    }
+    else if(level == 2){
+        enemyTex = LoadTexture("resource/BlueDemonFinal.png");
+        bgTex    = LoadTexture("resource/BlueBGFinal.png");
+        arrowTex = LoadTexture("resource/BlueArrow.png");
+        enemyspawner  = 1.3f;
         enemyBodySize = 80;
-        enemyValue = 3;
+        enemyValue    = 3;
         C = BLUE;
     }
-    else if(level==3){
-        enemyTex = enemyTex = LoadTexture("resource/RedDemonFinal.png");
-        bgTex = LoadTexture("resource/RedBGFinal.png");
-        enemyspawner = 1.1;
+    else if(level == 3){
+        enemyTex = LoadTexture("resource/RedDemonFinal.png");
+        bgTex    = LoadTexture("resource/RedBGFinal.png");
+        arrowTex = LoadTexture("resource/RedArrow.png");
+        enemyspawner  = 1.1f;
         enemyBodySize = 120;
-        enemyValue = 5;
+        enemyValue    = 5;
         C = RED;
     }
-    
-    vector<string>Words;
 
+    vector<string> Words;
     ifstream wordfile("words.txt");
     if(wordfile.is_open()){
         string line;
@@ -126,103 +149,170 @@ void RunBattle(){
     }
     wordfile.close();
 
+    Color Background = {20, 160, 193, 255};
+    Player player;
+    vector<enemy> v;
+    vector<Bullet> bullets;
+    float bulletSpeed = 600.0f;
 
-    Color Background = {20 , 160, 193, 255};
-    BattlePlayer player;
-    vector<enemy>v;
-
-    bool gameOver=false;
-
+    bool gameOver = false;
     int score = 0;
+    int enemyIdCounter = 0;
 
     while(!WindowShouldClose()){
 
-        time = time + GetFrameTime();
+        time += GetFrameTime();
         if(time >= enemyspawner){
             time = 0;
             enemy NewEnemy;
             NewEnemy.tex = &enemyTex;
+            NewEnemy.id  = enemyIdCounter++;
 
-            if(level==1){
-                NewEnemy.frame = 0;
-                NewEnemy.frameTimer = 0;
-                NewEnemy.frameSpeed = 0.1f;
-                NewEnemy.totalFrames = 16;
-                NewEnemy.cols = 4;           
-                NewEnemy.frameW = 200;       
-                NewEnemy.frameH = 200;             
+            if(level == 1){
+                NewEnemy.frameSpeed   = 0.1f;
+                NewEnemy.totalFrames  = 16;
+                NewEnemy.cols         = 4;
+                NewEnemy.frameW       = 200;
+                NewEnemy.frameH       = 200;
             }
-            else if(level==3){
-                NewEnemy.frame = 0;
-                NewEnemy.frameTimer = 0;
-                NewEnemy.frameSpeed = 0.1f;
-                NewEnemy.totalFrames = 4;
-                NewEnemy.cols = 2;           
-                NewEnemy.frameW = 100;       
-                NewEnemy.frameH = 100;                
+            else if(level == 2){
+                NewEnemy.frameSpeed   = 0.1f;
+                NewEnemy.totalFrames  = 16;
+                NewEnemy.cols         = 4;
+                NewEnemy.frameW       = 200;
+                NewEnemy.frameH       = 200;
+            }
+            else if(level == 3){
+                NewEnemy.frameSpeed   = 0.1f;
+                NewEnemy.totalFrames  = 4;
+                NewEnemy.cols         = 2;
+                NewEnemy.frameW       = 100;
+                NewEnemy.frameH       = 100;
             }
 
-            int i = GetRandomValue(0,Words.size()-1);
-            NewEnemy.s = Words[i];
-            float valY = GetRandomValue(50,750);
+            int i = GetRandomValue(0, Words.size()-1);
+            NewEnemy.s      = Words[i];
+            NewEnemy.health = Words[i].length();
+            float valY      = GetRandomValue(50, 750);
             float textwidth = MeasureText(Words[i].c_str(), 20);
-            NewEnemy.body = {800, valY, enemyBodySize, enemyBodySize};
-            NewEnemy.text = {800, valY-20, textwidth+10 , 20};
+            NewEnemy.body   = {800, valY, enemyBodySize, enemyBodySize};
+            NewEnemy.text   = {800, valY - 20, textwidth + 10, 20};
             float distanceX = -750;
             float distanceY = 500 - valY;
-            float distance  = sqrt((distanceX * distanceX)  + (distanceY * distanceY));
-            NewEnemy.speedX = (distanceX / distance) * 100 ;
-            NewEnemy.speedY = (distanceY / distance) * 100 ;
+            float distance  = sqrt((distanceX * distanceX) + (distanceY * distanceY));
+            NewEnemy.speedX = (distanceX / distance) * 100;
+            NewEnemy.speedY = (distanceY / distance) * 100;
             v.push_back(NewEnemy);
         }
 
-
         if(!gameOver){
-            for(int i = 0; i<(int)v.size() ; i++){
+
+            for(int i = 0; i < (int)v.size(); i++){
                 v[i].update();
-                    if(CheckCollisionRecs(v[i].body, player.body)){
-                        gameOver = true;
+                if(CheckCollisionRecs(v[i].body, player.body)){
+                    gameOver = true;
+                    break;
+                }
+            }
+
+            for(int i = 0; i < (int)bullets.size(); i++){
+                bullets[i].update();
+
+                for(int j = 0; j < (int)v.size(); j++){
+                    if(CheckCollisionRecs(bullets[i].body, v[j].body)){
+                        bullets[i].active = false;
+                        v[j].health--;
+                        if(v[j].health <= 0){
+                            v[j].active = false;
+                        }
+                        break;
+                    }
+                }
+
+                if(bullets[i].body.x > 800 || bullets[i].body.x < 0 ||
+                   bullets[i].body.y > 800 || bullets[i].body.y < 0){
+                    bullets[i].active = false;
+                }
+            }
+
+            for(int i = (int)bullets.size()-1; i >= 0; i--){
+                if(!bullets[i].active) bullets.erase(bullets.begin() + i);
+            }
+
+            for(int i = (int)v.size()-1; i >= 0; i--){
+                if(!v[i].active){
+                    v.erase(v.begin() + i);
+                    score += enemyValue;
+                    if(locked_index == i){
+                        locked = false;
+                        locked_index = -1;
+                    } else if(locked_index > i){
+                        locked_index--;
+                    }
                 }
             }
         }
         else{
-            if (IsKeyPressed(KEY_ENTER)) {
-                gameOver = false;
-                score = 0;
+            if(IsKeyPressed(KEY_ENTER)){
+                gameOver     = false;
+                score        = 0;
+                locked       = false;
+                locked_index = -1;
                 v.clear();
+                bullets.clear();
                 time = 1.9f;
-            }   
+            }
         }
-        int typed = GetCharPressed();
-        
-        while(typed>0){
 
+        int typed = GetCharPressed();
+        while(typed > 0){
             char letter = (char)typed;
+
             if(!locked){
-                for(int i=0;i<(int)v.size();i++){
-                    if(letter == v[i].s[0]){
-                        locked_index  = i;
+                for(int i = 0; i < (int)v.size(); i++){
+                    if(v[i].currentIndex < (int)v[i].s.length() && letter == v[i].s[v[i].currentIndex]){
+                        locked_index = i;
                         locked = true;
-                        v[i].s.erase(0,1);
-                        if(v[i].s.empty()){
+
+                        Bullet b;
+                        b.targetId = v[i].id;
+                        b.tex = &arrowTex;
+                        b.body = {player.body.x + 60, player.body.y + 25, 80, 40};
+                        float diffX = v[i].body.x - player.body.x;
+                        float diffY = v[i].body.y - player.body.y;
+                        float dist  = sqrt((diffX * diffX) + (diffY * diffY));
+                        b.speedX = (diffX / dist) * bulletSpeed;
+                        b.speedY = (diffY / dist) * bulletSpeed;
+                        bullets.push_back(b);
+
+                        v[i].currentIndex++;
+                        if(v[i].currentIndex >= (int)v[i].s.length()){
                             locked = false;
                             locked_index = -1;
-                            v.erase(v.begin() + i);
-                            score += enemyValue;
                         }
-                        break;        
+                        break;
                     }
                 }
             }
-
             else{
-                if(letter == v[locked_index].s[0] ){
-                    v[locked_index].s.erase(0,1);
+                if(v[locked_index].currentIndex < (int)v[locked_index].s.length() &&
+                   letter == v[locked_index].s[v[locked_index].currentIndex]){
+
+                    Bullet b;
+                    b.targetId = v[locked_index].id;
+                    b.tex = &arrowTex;
+                    b.body = {player.body.x + 60, player.body.y + 25, 80, 40};
+                    float diffX = v[locked_index].body.x - player.body.x;
+                    float diffY = v[locked_index].body.y - player.body.y;
+                    float dist  = sqrt((diffX * diffX) + (diffY * diffY));
+                    b.speedX = (diffX / dist) * bulletSpeed;
+                    b.speedY = (diffY / dist) * bulletSpeed;
+                    bullets.push_back(b);
+
+                    v[locked_index].currentIndex++;
                 }
-                if(v[locked_index].s.empty()){
+                if(v[locked_index].currentIndex >= (int)v[locked_index].s.length()){
                     locked = false;
-                    v.erase(v.begin() + locked_index);
-                    score += enemyValue;
                     locked_index = -1;
                 }
             }
@@ -230,41 +320,44 @@ void RunBattle(){
             typed = GetCharPressed();
         }
 
-
         BeginDrawing();
         ClearBackground(Background);
-        if(!gameOver){
 
+        if(!gameOver){
             DrawTexturePro(bgTex, {0,0,(float)bgTex.width,(float)bgTex.height}, {0,0,800,800}, {0,0}, 0.0f, WHITE);
 
             player.draw();
-            for(int i = 0; i<(int)v.size() ; i++){
-                v[i].draw(C);
-            }
+
+            for(int i = 0; i < (int)bullets.size(); i++) bullets[i].draw();
+            for(int i = 0; i < (int)v.size(); i++)       v[i].draw(C);
+
             string leveltext = "LEVEL " + to_string(level);
-            int levelWidth = MeasureText(leveltext.c_str(), 30);
-            DrawText(leveltext.c_str() , (800-levelWidth)/2 , 60, 30, DARKBLUE);
+            int levelWidth   = MeasureText(leveltext.c_str(), 30);
+            DrawText(leveltext.c_str(), (800-levelWidth)/2, 60, 30, DARKBLUE);
+
+            string scoretext = "Score: " + to_string(score);
+            DrawText(scoretext.c_str(), 10, 10, 25, WHITE);
         }
         else{
             int gameOverWidth = MeasureText("GAME OVER", 60);
-            DrawText("GAME OVER" , (800-gameOverWidth)/2 , 300, 60, DARKBLUE);
+            DrawText("GAME OVER", (800-gameOverWidth)/2, 300, 60, DARKBLUE);
 
             string scoretext = "Score: " + to_string(score);
-            int scoreWidth = MeasureText(scoretext.c_str(), 30);
+            int scoreWidth   = MeasureText(scoretext.c_str(), 30);
             DrawText(scoretext.c_str(), (800-scoreWidth)/2, 380, 30, DARKBLUE);
 
             int restartWidth = MeasureText("Press ENTER to Restart", 20);
-            DrawText("Press ENTER to Restart", (800 - restartWidth) / 2, 430, 20, DARKBLUE);
+            DrawText("Press ENTER to Restart", (800-restartWidth)/2, 430, 20, DARKBLUE);
         }
+
         EndDrawing();
     }
+
+    UnloadTexture(arrowTex);
     UnloadTexture(enemyTex);
     UnloadTexture(bgTex);
-
-
-    SetWindowSize(800, 600);
-    SetWindowTitle("game");
+    CloseWindow();
+    return 0;
 }
-
 
 // g++ PrototypeGame.cpp -o PrototypeGame -lraylib -lm -lpthread -ldl && ./PrototypeGame
